4734df3118c54f2f4a0351e99a9a875ea7a57359
JeRM <79025708+batata902@users.noreply.github.com>
2026-09-16 00:01:55 -0300

    edição no entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,10 @@
 
 set -e
 
-echo "Iniciando serviço local..."
-
 (
     cd /bank/bank
     python3 main.py
 ) &
-
-SERVICE_PID=$!
-
-echo "Serviço iniciado: $SERVICE_PID"
-
-echo "Iniciando aplicação web..."
 
 cd /bank/web
 
