b91299ca756133c3a24446e88aae74758e070fcf
Batata <aura@email.com>
2026-09-22 10:56:07 -0300

    Correções no setup

--- bank/src/__init__.py
+++ bank/src/__init__.py
@@ -1,12 +1,16 @@
 from src.models import User, Transfers, Cards
 from src.core import Bank
 import sqlite3
+from pathlib import Path
 
 bank: Bank = Bank()
 
 from src.commands import *
 
 def seed_database():
+    path = Path('src/database')
+    path.mkdir(parents=True, exist_ok=True)
+
     conn = sqlite3.connect('src/database/database.db')
     conn.executescript(open('schema.sql', 'r').read())
 
